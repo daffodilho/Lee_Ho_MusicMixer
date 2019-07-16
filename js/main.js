e57f2f0b1923d565902e1e130569ee05977ae910
daffodilho <46694934+daffodilho@users.noreply.github.com>
2019-07-16 16:16:18 -0400

    modified js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
-(() => {
+(()=>{
 	console.log("Trevor");
-})();
 
-const bottleCan = document.querySelector(".can"),
-	  dropZone =  document.querySelector("#zone");
+
+	const sodaCan = document.querySelector(".can"),
+		  dropZone =  document.querySelectorAll("#dropzone");
+	
+	dropZone.forEach (zone =>{
+		zone.addEventListener("dragover", function(e) {
+			e.preventDefault(); 
+            console.log('dragged sumpin over me');
+		});
+	
+			zone.addEventListener("drop", function(e) {
+            	e.preventDefault(); // don't do your default behaviour,
+            	// instead, do the following:
+            
+            	console.log('you dropped sumpin on me');
+			
+				let draggedElement = e.dataTransfer.getData("text/plain");
+            	console.log('you dragged: ', draggedElement);
+				e.target.appendChild(document.querySelector(`#${draggedElement}`));
+			});
+	
+	})
+	
+})();
